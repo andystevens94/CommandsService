4459c93c3eee0dd95b89bb457be2797407b50e56
Andy Stevens <26678082+andystevens94@users.noreply.github.com>
2022-11-21 20:35:06 +0000

    commit after time off

---
+++ Controllers/PlatformsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CommandsService.Controllers
+{
+	[Route("api/c/[controller]")]
+	[ApiController]
+	public class PlatformsController : ControllerBase
+	{
+		public PlatformsController()
+		{
+
+		}
+
+		[HttpPost]
+		public ActionResult TestInboundConnection()
+		{
+			Debug.WriteLine("Command Service - Inbound post");
+			return Ok("test");
+		}
+	}
+}
